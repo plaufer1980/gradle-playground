e8d07000501e541e4ca6340dc8e87ee591e32a22
Peter Laufer <plaufer@team.mobile.de>
2014-09-27 22:17:27 +0200

    added gitignore and example for single test execution

---
+++ java-library/src/test/java/SingleTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SingleTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
